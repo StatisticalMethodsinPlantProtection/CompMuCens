7604cec8901be2b692477fac28f7a8bc04bbbc1a
oe1114 <71254785+oe1114@users.noreply.github.com>
2023-06-20 14:07:05 +0800

    Update R_code.r

--- R_code.r
+++ R_code.r
@@ -4,7 +4,7 @@ CompMuCens <- function(dat, scale, grade=T, ckData=F){
     if (class(try(library(Icens))) =="try-error") {  
       if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
-      #Installing the Icens package which be required by interval package 
+      #Installing the Icens package, which is required by the Interval package
       BiocManager::install("Icens")
     }
     #Installing the interval package
